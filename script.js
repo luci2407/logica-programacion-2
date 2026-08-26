f81f517c019db257c2ccf6da0dd35f03999e361b
Lucila <luci.abi2407@gmail.com>
2026-08-26 17:24:33 -0600

    Cambios en el javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+let Celsius = prompt("Temperatura en grados Celsius");
+if(Celsius = Number){
+    let Fahrenheit = Celsius * 32
+    let Kelvin = Celsius * 273.15
+    console.log(Fahrenheit)
+    console.log(Kelvin)
+} else(console.log("Inserta un dato valido"))
